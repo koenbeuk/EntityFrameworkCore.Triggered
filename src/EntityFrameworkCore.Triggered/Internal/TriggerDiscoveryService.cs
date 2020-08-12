cc3e546856cb3130d69eb1422e364f873d8c38e4
Koen Bekkenutte <2912652+kbekkenutte@users.noreply.github.com>
2020-08-13 01:43:03 +0800

    Ability to capture and use an external service provider when raising triggers

--- src/EntityFrameworkCore.Triggered/Internal/TriggerDiscoveryService.cs
+++ src/EntityFrameworkCore.Triggered/Internal/TriggerDiscoveryService.cs
@@ -2,6 +2,9 @@
 using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace EntityFrameworkCore.Triggered.Internal
@@ -11,17 +14,28 @@ namespace EntityFrameworkCore.Triggered.Internal
         readonly ITriggerServiceProviderAccessor _triggerServiceProviderAccessor;
         readonly ITriggerTypeRegistryService _triggerTypeRegistryService;
 
+        IServiceProvider? _serviceProvider;
+
         public TriggerDiscoveryService(ITriggerServiceProviderAccessor triggerServiceProviderAccessor, ITriggerTypeRegistryService triggerTypeRegistryService)
         {
             _triggerServiceProviderAccessor = triggerServiceProviderAccessor ?? throw new ArgumentNullException(nameof(triggerServiceProviderAccessor));
             _triggerTypeRegistryService = triggerTypeRegistryService ?? throw new ArgumentNullException(nameof(triggerTypeRegistryService));
         }
 
-
         public IEnumerable<TriggerDescriptor> DiscoverTriggers(Type openTriggerType, Type entityType, Func<Type, ITriggerTypeDescriptor> triggerTypeDescriptorFactory)
         {
+            IServiceProvider serviceProvider;
+
+            if (_serviceProvider == null)
+            {
+                serviceProvider = _triggerServiceProviderAccessor.GetTriggerServiceProvider();
+            }
+            else
+            {
+                serviceProvider = _serviceProvider;
+            }
+
             var registry = _triggerTypeRegistryService.ResolveRegistry(openTriggerType, entityType, triggerTypeDescriptorFactory);
-            var serviceProvider = _triggerServiceProviderAccessor.GetTriggerServiceProvider();
 
             return registry.GetTriggerTypeDescriptors()
                 .SelectMany(triggerTypeDescriptor =>
@@ -31,6 +45,16 @@ namespace EntityFrameworkCore.Triggered.Internal
                         .Select(trigger => new TriggerDescriptor(triggerTypeDescriptor, trigger))
                         .OrderBy(x => x.Priority)
                 );
+        }
+
+        public void SetServiceProvider(IServiceProvider serviceProvider)
+        {
+            if (_serviceProvider != null)
+            {
+                throw new InvalidOperationException("Service provider needs to be set before discovery has started");
+            }
+
+            _serviceProvider = serviceProvider;
         }
     }
 }
--- test/EntityFrameworkCore.Triggered.Tests/Stubs/TriggerServiceStub.cs
+++ test/EntityFrameworkCore.Triggered.Tests/Stubs/TriggerServiceStub.cs
@@ -11,10 +11,12 @@ namespace EntityFrameworkCore.Triggered.Tests.Stubs
     public class TriggerServiceStub : ITriggerService
     {
         public int CreateSessionCalls;
+        public IServiceProvider ServiceProvider;
 
-        public ITriggerSession CreateSession(DbContext context)
+        public ITriggerSession CreateSession(DbContext context, IServiceProvider serviceProvider)
         {
             CreateSessionCalls += 1;
+            ServiceProvider = serviceProvider;
             return new TriggerSessionStub();
         }
     }

d4d2782188a5e73287b69c4ffba80323bdf421f8
Koen Bekkenutte <2912652+kbekkenutte@users.noreply.github.com>
2020-08-12 16:13:19 +0800

    Added basic benchmarks to measure overhead

---
+++ benchmarks/EntityFrameworkCore.Triggered.Benchmarks/Triggers/SetStudentRegistrationDateTrigger.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace EntityFrameworkCore.Triggered.Benchmarks.Triggers
+{
+    public class SetStudentRegistrationDateTrigger : IBeforeSaveTrigger<Student>
+    {
+        public Task BeforeSave(ITriggerContext<Student> context, CancellationToken cancellationToken)
+        {
+            context.Entity.RegistrationDate = DateTimeOffset.Now;
+            return Task.CompletedTask;
+        }
+    }
+}

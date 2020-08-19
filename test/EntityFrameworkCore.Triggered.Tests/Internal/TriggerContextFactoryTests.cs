367ca5c9c843012ed674747fa4e138b4def8ea53
Koen Bekkenutte <2912652+kbekkenutte@users.noreply.github.com>
2020-08-19 15:51:08 +0800

    Unit test to catch a bug

--- test/EntityFrameworkCore.Triggered.Tests/Internal/TriggerContextFactoryTests.cs
+++ test/EntityFrameworkCore.Triggered.Tests/Internal/TriggerContextFactoryTests.cs
@@ -4,12 +4,31 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using EntityFrameworkCore.Triggered.Internal;
+using Microsoft.EntityFrameworkCore;
 using Xunit;
 
 namespace EntityFrameworkCore.Triggered.Tests.Internal
 {
     public class TriggerContextFactoryTests
     {
+        class TestModel
+        {
+            public Guid Id { get; set; }
+            public string Name { get; set; }
+        }
+
+        class TestDbContext : DbContext
+        {
+            public DbSet<TestModel> TestModels { get; set; }
+
+            protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+            {
+                base.OnConfiguring(optionsBuilder);
+
+                optionsBuilder.UseInMemoryDatabase(nameof(TriggerContextFactoryTests));
+            }
+        }
+
         [Fact]
         public void Activate_ReturnsInstance()
         {
@@ -17,6 +36,5 @@ namespace EntityFrameworkCore.Triggered.Tests.Internal
 
             Assert.NotNull(result);
         }
-
     }
 }

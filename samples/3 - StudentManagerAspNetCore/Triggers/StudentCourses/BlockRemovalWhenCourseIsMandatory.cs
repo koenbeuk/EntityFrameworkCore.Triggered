efcd2791c3267075d14bf525f427aaa16281362d
Koen Bekkenutte <2912652+kbekkenutte@users.noreply.github.com>
2020-08-23 23:47:24 +0800

    Fixed a typo

---
+++ samples/3 - StudentManagerAspNetCore/Triggers/StudentCourses/BlockRemovalWhenCourseIsMandatory.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using EntityFrameworkCore.Triggered;
+
+namespace StudentManager.Triggers.StudentCourses
+{
+    public class BlockRemovalWhenCourseIsMandatory : IBeforeSaveTrigger<StudentCourse>
+    {
+        readonly ApplicationContext _applicationContext;
+
+        public BlockRemovalWhenCourseIsMandatory(ApplicationContext applicationContext)
+        {
+            _applicationContext = applicationContext;
+        }
+
+        public async Task BeforeSave(ITriggerContext<StudentCourse> context, CancellationToken cancellationToken)
+        {
+            if (context.ChangeType == ChangeType.Deleted)
+            {
+                var course = await _applicationContext.Courses.FindAsync(new object[] { context.Entity.CourseId }, cancellationToken);
+                if (course.IsMandatory)
+                {
+                    throw new InvalidOperationException("Course is required");
+                }
+            }
+        }
+    }
+}

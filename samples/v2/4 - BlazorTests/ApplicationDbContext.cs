86a46c7a04ad1b031e5594139c91f1021337832b
Koen Bekkenutte <2912652+kbekkenutte@users.noreply.github.com>
2020-11-19 23:34:01 +0800

    Initial blazor template project

---
+++ samples/v2/4 - BlazorTests/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorTests
+{
+    public class Count
+    {
+        public Guid Id { get; set; }
+        public DateTimeOffset CreatedOn { get; set; }
+    }
+
+    public class ApplicationDbContext : DbContext
+    {
+        
+    }
+}

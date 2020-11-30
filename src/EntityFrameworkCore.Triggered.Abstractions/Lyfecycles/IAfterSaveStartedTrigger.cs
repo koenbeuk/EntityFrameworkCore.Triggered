9ec4aab3dcfaf11eee636f07536e1f1acd1a9ea4
Koen Bekkenutte <2912652+kbekkenutte@users.noreply.github.com>
2020-12-01 02:01:56 +0800

    Added lifecycle trigger types

---
+++ src/EntityFrameworkCore.Triggered.Abstractions/Lyfecycles/IAfterSaveStartedTrigger.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+
+namespace EntityFrameworkCore.Triggered.Lyfecycles
+{
+    public interface IAfterSaveStartedTrigger
+    {
+        Task AfterSaveStarted(CancellationToken cancellationToken);
+    }
+}

f490b0e33ffec444f96737fee2eb81651192c479
Koen Bekkenutte <2912652+kbekkenutte@users.noreply.github.com>
2020-12-07 00:35:53 +0800

    Added transaction lifecycle triggers

---
+++ src/EntityFrameworkCore.Triggered.Transactions.Abstractions/Lifecycles/IAfterCommitStartedTrigger.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace EntityFrameworkCore.Triggered.Transactions.Abstractions.Lifecycles
+{
+    public interface IAfterCommitStartedTrigger
+    {
+        Task AfterCommitStarted(CancellationToken cancellationToken);
+    }
+}

7ccf8610018a876879e87bff53790bd4e4ae20a6
Koen Bekkenutte <2912652+kbekkenutte@users.noreply.github.com>
2021-04-12 16:18:31 +0800

    Initial functional test demo

---
+++ test/EntityFrameworkCore.Triggered.IntegrationTests/ApplicationDbContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EntityFrameworkCore.Triggered.IntegrationTests.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EntityFrameworkCore.Triggered.IntegrationTests
+{
+    public class ApplicationDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseInMemoryDatabase("test");
+        }
+
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+        }
+    }
+}
---
+++ test/EntityFrameworkCore.Triggered.IntegrationTests/TestScenario.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EntityFrameworkCore.Triggered.IntegrationTests;
+using EntityFrameworkCore.Triggered.IntegrationTests.Models;
+using Linker.ScenarioTests;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Xunit;
+
+namespace IntegrationTests
+{
+    public partial class TestScenario
+    {
+        [Scenario]
+        public void TestScenario1(ScenarioContext scenario)
+        {
+            const int sampleUsersCount = 100;
+
+            var dbContext = new ApplicationDbContext();
+
+            // step 1: Populate database with users
+            {
+                dbContext.Users.AddRange(Enumerable.Range(0, sampleUsersCount).Select(x => new User {
+                    UserName = $"user{x}"
+                }));
+             
+                dbContext.SaveChanges();
+            }
+
+            scenario.Fact("Database saved all users", () => {
+                var usersCount = dbContext.Users.Count();
+                Assert.Equal(sampleUsersCount, usersCount);
+            });
+        }
+    }
+}

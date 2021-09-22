841b63316b4a78d4efdb36e857a4bac65ac41db2
Koen Bekkenutte <2912652+kbekkenutte@users.noreply.github.com>
2021-09-23 00:17:20 +0800

    Minor doc additions

--- src/EntityFrameworkCore.Triggered.Abstractions/ITriggerContext.cs
+++ src/EntityFrameworkCore.Triggered.Abstractions/ITriggerContext.cs
@@ -5,10 +5,19 @@ namespace EntityFrameworkCore.Triggered
     public interface ITriggerContext<out TEntity>
         where TEntity : class
     {
+        /// <summary>
+        /// The type of change that caused this trigger to be raised 
+        /// </summary>
         ChangeType ChangeType { get; }
 
+        /// <summary>
+        /// The entity that caused this trigger to be raised
+        /// </summary>
         TEntity Entity { get; }
 
+        /// <summary>
+        /// This will retain the unmodified entity in case when the ChangeType is <c>Modified</c>
+        /// </summary>
         TEntity? UnmodifiedEntity { get; }
 
         /// <summary>

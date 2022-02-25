7f5e7fe4e8f1663f318f6002a874ccc6d1fca5db
aescg1220 <Cody.Gentry@aeshield.com>
2022-02-25 13:09:40 -0500

    updated servicecollection replace lifetime to use singleton if it was singleton.  otherwise use scoped.

--- src/EntityFrameworkCore.Triggered/Extensions/ServiceCollectionExtensions.cs
+++ src/EntityFrameworkCore.Triggered/Extensions/ServiceCollectionExtensions.cs
@@ -79,7 +79,7 @@ namespace Microsoft.Extensions.DependencyInjection
                 serviceCollection.Replace(ServiceDescriptor.Describe(
                     serviceType: typeof(IDbContextFactory<TContext>),
                     implementationFactory: serviceProvider => ActivatorUtilities.CreateInstance(serviceProvider, triggeredFactoryType, serviceProvider.GetRequiredService(serviceDescriptor.ImplementationType), serviceProvider),
-                    lifetime: ServiceLifetime.Scoped
+                    lifetime: lifetime == ServiceLifetime.Singleton ? lifetime : ServiceLifetime.Scoped
                 ));
             }
 
@@ -110,7 +110,7 @@ namespace Microsoft.Extensions.DependencyInjection
                 serviceCollection.Replace(ServiceDescriptor.Describe(
                     serviceType: typeof(IDbContextFactory<TContext>),
                     implementationFactory: serviceProvider => ActivatorUtilities.CreateInstance(serviceProvider, triggeredFactoryType, serviceProvider.GetRequiredService(serviceDescriptor.ImplementationType), serviceProvider),
-                    lifetime: ServiceLifetime.Scoped
+                    lifetime: lifetime == ServiceLifetime.Singleton ? lifetime : ServiceLifetime.Scoped
                 ));
             }
 

f734bb618b4cb5ce43c69206b48aaf57e8d5a511
Koen Bekkenutte <2912652+kbekkenutte@users.noreply.github.com>
2022-03-17 23:33:28 +0800

    Removed deprecated ASP.NET Core integation

--- test/EntityFrameworkCore.Triggered.AspNetCore.Tests/Stubs/HttpContextAccessorStub.cs
+++
@@ -1,9 +0,0 @@
-﻿using Microsoft.AspNetCore.Http;
-
-namespace EntityFrameworkCore.Triggered.AspNetCore.Tests.Stubs
-{
-    public class HttpContextAccessorStub : IHttpContextAccessor
-    {
-        public HttpContext HttpContext { get; set; } = new DefaultHttpContext();
-    }
-}

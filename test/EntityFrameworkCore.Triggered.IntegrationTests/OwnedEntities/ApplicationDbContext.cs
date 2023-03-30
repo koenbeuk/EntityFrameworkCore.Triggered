b4c6140953a5f77fa0dbe5b1d7c57215d4bd852b
Koen <koen@linker.io>
2023-03-31 00:44:05 +0100

    Added repro test

---
+++ test/EntityFrameworkCore.Triggered.IntegrationTests/OwnedEntities/ApplicationDbContext.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace EntityFrameworkCore.Triggered.IntegrationTests.OwnedEntities
+{
+    [Owned]
+    public class Ownee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+
+    public class Owner 
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public Ownee Ownee { get; set; }
+    }
+
+
+    class ApplicationDbContext : DbContext
+    {
+        readonly string _databaseName;
+
+        public ApplicationDbContext(string databaseName)
+        {
+            _databaseName = databaseName;
+        }
+
+        public OwneeTrigger OwneeTrigger { get; } = new OwneeTrigger();
+        public OwnerTrigger OwnerTrigger { get; } = new OwnerTrigger();
+
+        public DbSet<Owner> B => Set<Owner>();
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseInMemoryDatabase(_databaseName);
+            optionsBuilder.UseTriggers(triggerOptions => {
+                triggerOptions.AddTrigger(OwneeTrigger);
+                triggerOptions.AddTrigger(OwnerTrigger);
+            });
+        }
+    }
+
+    class OwneeTrigger : IBeforeSaveTrigger<Ownee>
+    {
+        public void BeforeSave(ITriggerContext<Ownee> context)
+        {
+            Calls++;
+        }
+
+        public int Calls { get; set; }
+    }
+
+    class OwnerTrigger : IBeforeSaveTrigger<Owner>
+    {
+        public void BeforeSave(ITriggerContext<Owner> context)
+        {
+            Calls++;
+        }
+
+        public int Calls { get; set; }
+    }
+}
---
+++ test/EntityFrameworkCore.Triggered.IntegrationTests/OwnedEntities/TestScenario.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using ScenarioTests;
+using Xunit;
+
+namespace EntityFrameworkCore.Triggered.IntegrationTests.OwnedEntities
+{
+    public partial class OwnedEntities
+    {
+        [Scenario(NamingPolicy = ScenarioTestMethodNamingPolicy.Test)]
+        public void TestScenario(ScenarioContext scenario)
+        {
+            var dbContext = new ApplicationDbContext(scenario.TargetName);
+
+            Assert.Equal(0, dbContext.OwneeTrigger.Calls);
+            Assert.Equal(0, dbContext.OwnerTrigger.Calls);
+
+            var owner = new Owner {
+                Ownee = new Ownee()
+            };
+
+            dbContext.Add(owner);
+
+            scenario.Fact("We can trigger both ownee and owner on save", () => {
+                dbContext.SaveChanges();
+
+                Assert.Equal(1, dbContext.OwneeTrigger.Calls);
+                Assert.Equal(1, dbContext.OwnerTrigger.Calls);
+            });
+
+            dbContext.SaveChanges();
+
+            scenario.Fact("We can update the ownee and trigger that", () => {
+                owner.Ownee.Name = "something new";
+                dbContext.SaveChanges();
+
+                Assert.Equal(2, dbContext.OwneeTrigger.Calls);
+                Assert.Equal(1, dbContext.OwnerTrigger.Calls);
+            });
+
+            scenario.Fact("We can update the owner and trigger that", () => {
+                owner.Name = "something new";
+                dbContext.SaveChanges();
+
+                Assert.Equal(1, dbContext.OwneeTrigger.Calls);
+                Assert.Equal(2, dbContext.OwnerTrigger.Calls);
+            });
+        }
+    }
+}
